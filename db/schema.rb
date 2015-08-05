5e0510807085cc5cd0ed9ac761c054897161fb1e
Jason Hamilton <jhamiltonpro@gmail.com>
2015-08-05 23:32:32 +0000

    Generates Portfolio scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805174728) do
+ActiveRecord::Schema.define(version: 20150805232915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,12 @@ ActiveRecord::Schema.define(version: 20150805174728) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id",     null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.integer  "contractor_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
