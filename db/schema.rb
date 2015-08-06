cef9e67feebbe806090906c7cf71298c0d8ed3ae
Jason Hamilton <jhamiltonpro@gmail.com>
2015-08-06 02:20:26 +0000

    Generates model's Skill, SkillSet and SkillTag and migrates.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806003931) do
+ActiveRecord::Schema.define(version: 20150806013658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,27 @@ ActiveRecord::Schema.define(version: 20150806003931) do
     t.integer  "contractor_id", null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "skill_sets", force: :cascade do |t|
+    t.string   "contractor_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "skill_tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.integer  "skill_set_id", null: false
+    t.integer  "skill_tag_id", null: false
+    t.integer  "years",        null: false
+    t.integer  "proficiency",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
